74f1ebd819abb858767b1ed6a1a5d9cc553448af
Jous99 <josemanuel7199@gmail.com>
2026-03-30 17:52:50 +0200

    delete everything to reupload

--- src/yuzu/overlay.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright 2020 yuzu Emulator Project
-// Licensed under GPLv2 or any later version
-// Refer to the license.txt file included.
-
-#include "yuzu/overlay.h"
-
-Overlay::Overlay(QWidget* parent) : QWidget(parent) {}
